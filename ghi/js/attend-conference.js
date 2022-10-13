5484016bafed7de0b544e838d0dce6622306d763
Lindsey <lindseykaycarlson@outlook.com>
2022-10-13 16:07:24 -0700

    addentee form and CORS

--- ghi/js/attend-conference.js
+++ ghi/js/attend-conference.js
@@ -16,6 +16,35 @@ window.addEventListener('DOMContentLoaded', async () => {
       const loadingTag = document.getElementById('loading-conference-spinner');
       selectTag.classList.remove('d-none');
       loadingTag.classList.add('d-none');
+
+
+
+        const formTag = document.getElementById('create-attendee-form');
+        formTag.addEventListener('submit', async event => {
+            event.preventDefault();
+            const formData = new FormData(formTag);
+            const json = JSON.stringify(Object.fromEntries(formData));
+
+            const attendeesUrl = "http://localhost:8001/api/attendees/";
+            const fetchConfig = {
+                method: "post",
+                body: json,
+                headers: {
+                    'Content-Type' : 'applications/json',
+                },
+            };
+            const response = await fetch(attendeesUrl, fetchConfig);
+            if (response.ok) {
+                formTag.reset();
+
+                const newAttendee = await response.json();
+                const successTag = document.getElementById('success-message');
+                const formTag2 = document.getElementById('create-attendee-form');
+
+                successTag.classList.remove("d-none");
+                formTag2.classList.add('d-none');
+                }
+            })
     }
 
   });
